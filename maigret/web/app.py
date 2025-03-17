46f24ccfa2f582c2dd74f9423aa2a5ffabe22f50
RobertocDev <54865495+RobertocDev@users.noreply.github.com>
2025-03-17 16:00:59 -0300

    Update app.py
    no debug_mode

--- maigret/web/app.py
+++ maigret/web/app.py
@@ -329,5 +329,5 @@ if __name__ == '__main__':
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     )
     debug_mode = os.getenv('FLASK_DEBUG', 'False').lower() in ['true', '1', 't']
-    app.run(debug=debug_mode)
+    app.run(host='0.0.0.0', port=5000, debug=debug_mode)
 #fim
