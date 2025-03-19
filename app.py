a12daf048fd86de5034c06bbbde9bc4f8315a45d
RobertocDev <wallacerobertoc@gmail.com>
2025-03-19 14:36:52 +0000

    001

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ import os
 
 app = Flask(__name__)
 
-# Rota para pesquisa
+@app.route('/')
+def home():
+    return "Bem-vindo ao Maigret Flask Backend! Use /search?username=SEU_USERNAME para pesquisar."
+
 @app.route('/search', methods=['GET'])
 def search():
     username = request.args.get('username')
@@ -13,9 +16,8 @@ def search():
         return jsonify({"error": "O parâmetro 'username' é obrigatório."}), 400
 
     try:
-        # Executa o maigret
         result = subprocess.run(
-            ["maigret", username, "-J", "simple", "--top-sites", "10"],
+            ["maigret", username, "-J", "simple", "--top-sites", "20"],
             capture_output=True,
             text=True,
             check=True
@@ -23,14 +25,11 @@ def search():
         print("Resultado do maigret (stdout):", result.stdout)  # Depuração
         print("Erros do maigret (stderr):", result.stderr)      # Depuração
 
-        # Caminho do arquivo JSON gerado pelo maigret
         json_file_path = f"/workspaces/maigret/reports/report_{username}_simple.json"
 
-        # Verifica se o arquivo JSON existe
         if not os.path.exists(json_file_path):
             return jsonify({"error": "Arquivo JSON não encontrado."}), 500
 
-        # Lê o conteúdo do arquivo JSON
         with open(json_file_path, "r") as file:
             json_result = json.load(file)
 
@@ -42,6 +41,5 @@ def search():
     except Exception as e:
         return jsonify({"error": f"Erro inesperado: {str(e)}"}), 500
 
-# Inicia o servidor Flask
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
